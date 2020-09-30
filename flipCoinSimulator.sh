4ace45838d53708106f177f54bf47221839ef143
Suraj <surajjadhav9741@gmail.com>
2020-09-30 14:31:51 +0530

    Modify : UC-2 add condition in head and tail either of them have won 21 time and display result

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,18 +3,34 @@ loop=0
 Head=0
 Tail=0
 flip=21
-while [ $loop -lt $flip ]
+while (( $Head < $flip && $Tail < $flip ))
 do
 	n=$((RANDOM%2))
 	if [ $n -eq 1 ]
 	then
-		echo "Head Winner."
+		#echo "Head Winner."
 		Head=$((Head+1))
-	else
-		echo "Tail Winner."
+	elif [ $n -eq 0 ]
+	then
+		#echo "Tail Winner."
 		Tail=$((Tail+1))
 	fi
 	loop=$((loop+1))
 done
-echo "Total Head Wins out of $flip is $Head"
-echo "Total Tail Wins out of $flip is $Tail"
+if [ $Head -gt $Tail ]
+then
+	echo "Head Winner "
+	echo "difference between Head and Tail is $((Head-Tail))"
+	echo "Total Head Wins out of $loop is $Head"
+	echo "Total Tail Wins out of $loop is $Tail"
+elif [ $Head -lt $Tail ]
+then
+	echo "Tail Winner "
+        echo "difference between Head and Tail is $((Tail-Head))"
+	echo "Total Tail Wins out of $loop is $Tail"
+	echo "Total Head Wins out of $loop is $Head"
+else
+	echo "Tie game"
+	echo "Total Head Wins out of $loop is $Head"
+        echo "Total Tail Wins out of $loop is $Tail"
+fi
