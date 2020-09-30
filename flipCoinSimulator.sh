c6c86b01866c9115b34a724bfa05b2052b5b6e1c
Suraj <surajjadhav9741@gmail.com>
2020-09-30 14:49:48 +0530

    Add : If game tie then the game continues till the difference of minimum 2 points achieved

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -17,6 +17,7 @@ do
 	fi
 	loop=$((loop+1))
 done
+#Head=$Tail
 if [ $Head -gt $Tail ]
 then
 	echo "Head Winner "
@@ -33,4 +34,33 @@ else
 	echo "Tie game"
 	echo "Total Head Wins out of $loop is $Head"
         echo "Total Tail Wins out of $loop is $Tail"
+	while (($((Head-Tail))<=2))
+	do
+		n=$((RANDOM%2))
+        	if [ $n -eq 1 ]
+        	then
+        	        #echo "Head Winner."
+        	        Head=$((Head+1))
+        	elif [ $n -eq 0 ]
+        	then
+        	        #echo "Tail Winner."
+        	        Tail=$((Tail+1))
+	        fi
+	        loop=$((loop+1))
+	done
+	echo "if while done"
+	if [ $Head -gt $Tail ]
+	then
+	        echo "Head Winner "
+	        echo "difference between Head and Tail is $((Head-Tail))"
+	        echo "Total Head Wins out of $loop is $Head"
+	        echo "Total Tail Wins out of $loop is $Tail"
+	elif [ $Head -lt $Tail ]
+	then
+        	echo "Tail Winner "
+        	echo "difference between Head and Tail is $((Tail-Head))"
+        	echo "Total Tail Wins out of $loop is $Tail"
+        	echo "Total Head Wins out of $loop is $Head"
+	fi
+
 fi
