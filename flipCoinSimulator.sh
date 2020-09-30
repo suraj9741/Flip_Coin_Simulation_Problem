ddc3ccbea61a8ab38f2898c032e18749ee17dbd1
Suraj <surajjadhav9741@gmail.com>
2020-09-30 15:22:59 +0530

    Remove : Raw code

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,64 +3,65 @@ loop=0
 Head=0
 Tail=0
 flip=21
+coin_flip()
+{
+	n=$((RANDOM%2))
+        if [ $n -eq 1 ]
+        then
+                #echo "Head Winner."
+                Head=$((Head+1))
+        elif [ $n -eq 0 ]
+        then
+                #echo "Tail Winner."
+                Tail=$((Tail+1))
+        fi
+        loop=$((loop+1))
+}
+check_diff()
+{
+	if [ $Head -gt $Tail ]
+	then
+		diff=$((Head-Tail))
+	else
+		diff=$((Tail-Head))
+	fi
+}
+print()
+{
+	check_diff
+	echo "difference between Head and Tail is $diff"
+        echo "Total Tail Wins out of $loop is $Tail"
+        echo "Total Head Wins out of $loop is $Head"
+}
 while (( $Head < $flip && $Tail < $flip ))
 do
-	n=$((RANDOM%2))
-	if [ $n -eq 1 ]
-	then
-		#echo "Head Winner."
-		Head=$((Head+1))
-	elif [ $n -eq 0 ]
-	then
-		#echo "Tail Winner."
-		Tail=$((Tail+1))
-	fi
-	loop=$((loop+1))
+        coin_flip
 done
-#Head=$Tail
 if [ $Head -gt $Tail ]
 then
 	echo "Head Winner "
-	echo "difference between Head and Tail is $((Head-Tail))"
-	echo "Total Head Wins out of $loop is $Head"
-	echo "Total Tail Wins out of $loop is $Tail"
+	print
 elif [ $Head -lt $Tail ]
 then
 	echo "Tail Winner "
-        echo "difference between Head and Tail is $((Tail-Head))"
-	echo "Total Tail Wins out of $loop is $Tail"
-	echo "Total Head Wins out of $loop is $Head"
+	print
 else
 	echo "Tie game"
-	echo "Total Head Wins out of $loop is $Head"
-        echo "Total Tail Wins out of $loop is $Tail"
-	while (($((Head-Tail))<=2))
+	print
+	while (($diff<=2))
 	do
-		n=$((RANDOM%2))
-        	if [ $n -eq 1 ]
-        	then
-        	        #echo "Head Winner."
-        	        Head=$((Head+1))
-        	elif [ $n -eq 0 ]
-        	then
-        	        #echo "Tail Winner."
-        	        Tail=$((Tail+1))
-	        fi
-	        loop=$((loop+1))
+		coin_flip
+		check_diff
 	done
 	echo "if while done"
 	if [ $Head -gt $Tail ]
 	then
 	        echo "Head Winner "
-	        echo "difference between Head and Tail is $((Head-Tail))"
-	        echo "Total Head Wins out of $loop is $Head"
-	        echo "Total Tail Wins out of $loop is $Tail"
+	        print
 	elif [ $Head -lt $Tail ]
 	then
         	echo "Tail Winner "
-        	echo "difference between Head and Tail is $((Tail-Head))"
-        	echo "Total Tail Wins out of $loop is $Tail"
-        	echo "Total Head Wins out of $loop is $Head"
+        	print
 	fi
 
 fi
