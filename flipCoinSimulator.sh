cd705fec37465ab32dde932e70d4af62373479e8
Suraj <surajjadhav9741@gmail.com>
2020-09-30 12:17:04 +0530

    Add : Loop trough Multiple times of flipping a coin and show number of times head and tail has won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,20 @@
 ##!/bin/bash -x
-n=$((RANDOM%2))
-if [ $n -eq 1 ]
-then
-	echo "Heads Winner."
-else
-	echo "Tails Winner."
-fi
+loop=0
+Head=0
+Tail=0
+flip=21
+while [ $loop -lt $flip ]
+do
+	n=$((RANDOM%2))
+	if [ $n -eq 1 ]
+	then
+		echo "Head Winner."
+		Head=$((Head+1))
+	else
+		echo "Tail Winner."
+		Tail=$((Tail+1))
+	fi
+	loop=$((loop+1))
+done
+echo "Total Head Wins out of $flip is $Head"
+echo "Total Tail Wins out of $flip is $Tail"
